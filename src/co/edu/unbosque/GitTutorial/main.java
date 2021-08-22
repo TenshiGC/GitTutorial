8bcfe56a136600ac819890bfe7fa617012cd92b8
TenshiGC <81397795+TenshiGC@users.noreply.github.com>
2021-08-22 17:09:55 -0500

    commit since git

--- src/co/edu/unbosque/GitTutorial/main.java
+++ src/co/edu/unbosque/GitTutorial/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Void");
+		System.out.println("Hello Void, fuck this shit");
 	}
 
 }
