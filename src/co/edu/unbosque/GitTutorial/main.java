e43ee99d8693277207486b9a0a261fc9841eb28a
TenshiGC <tauroangel.ruiz@gmail.com>
2021-08-22 17:02:38 -0500

    Try 1

---
+++ src/co/edu/unbosque/GitTutorial/main.java
@@ -0,0 +1,10 @@
+package co.edu.unbosque.GitTutorial;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Void");
+	}
+
+}
